df0d4b664d29917c98ce20e105a6db62a73bc028
levinsseri <ivanblintsov8@gmail.com>
2026-09-04 18:02:28 +0300

    Update FastFlagManager.cs

--- Bloxstrap/FastFlagManager.cs
+++ Bloxstrap/FastFlagManager.cs
@@ -130,9 +130,18 @@ namespace Bloxstrap
         public override void Save()
         {
             // convert all flag values to strings before saving
+            // null values can exist in hand-edited or older flag files;
+            // a null value means that the flag should be removed.
 
-            foreach (var pair in Prop)
-                Prop[pair.Key] = pair.Value.ToString()!;
+            foreach (string key in Prop.Keys.ToArray())
+            {
+                object? value = Prop[key];
+
+                if (value is null)
+                    Prop.Remove(key);
+                else
+                    Prop[key] = value.ToString();
+            }
 
             base.Save();
 
